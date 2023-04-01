0e91633e0339341570553c6ea660a126e0180b40
TAESUNG LEE <ts.lee@vtpl.kr>
2023-04-01 15:00:44 +0900

    [ADD]_lts : Variables and Constants

--- 1.THEORY/var_const.go
+++ 1.THEORY/var_const.go
@@ -6,8 +6,25 @@ import "fmt"
 func printVariable() {
 	var name bool = false
 	name = true
-
-	// name := true
-	// name = false
 	fmt.Println(name)
+}
+
+func printConstant() {
+	const name bool = false
+	fmt.Println(name)
+}
+
+func printAbbreviation() {
+	bool := false // bool is a keyword
+	bool = true
+	// bool = 1 // Error
+	// bool = "string" // Error
+	// bool = 1.0 // Error
+	fmt.Println(bool)
+}
+
+func PrintAll() {
+	printVariable()
+	printConstant()
+	printAbbreviation()
 }
